7ce4c87d1e8fa23051a6de1b6216e358bb3af0f2
Ryan Baumann <ryan.baumann@gmail.com>
2010-08-04 10:53:43 -0400

    Stub main

--- xyzfit.cpp
+++ xyzfit.cpp
@@ -30,3 +30,28 @@ float colorchecker_xyz[24][3] = {
 	{7.93386184429607,8.56775332784279,10.818847695165 },
 	{2.80207274806336,3.01238408341872,3.82277704246866}
 };
+
+int main( int argc, char *argv[] )
+{
+    if( argc < 2 )
+    {
+        fprintf( stderr, "Usage: %s macbeth_locations.csv [input_image_1 input_image_2 input_image_3 ...]\n", argv[0] );
+        return 1;
+    }
+
+	int n = argc - 2;
+	IplImage ** input_channels = (IplImage**)malloc(n*sizeof(IplImage**));
+	
+	for(int i = 0; i < n; i++) {
+		printf("Loading channel %d (%s)\n", i, argv[i+2]);
+		input_channels[i] = cvLoadImage( argv[i+2],
+	        CV_LOAD_IMAGE_ANYCOLOR|CV_LOAD_IMAGE_ANYDEPTH );
+	}
+	
+	printf("Releasing images...\n");
+	for(int i = 0; i < n; i++) {
+		cvReleaseImage( &(input_channels[i]) );
+	}
+
+	return 0;
+}
