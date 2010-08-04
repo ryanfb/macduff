8bbcf33c98424e93f16af12f70bb9a60220b82be
Ryan Baumann <ryan.baumann@gmail.com>
2010-08-04 11:05:12 -0400

    Split shared colorchecker info

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: macduff xyzfit
 
-macduff: macduff.cpp
+macduff: macduff.cpp colorchecker.h
 	g++ -I/opt/local/include -L/opt/local/lib macduff.cpp -lcvaux -lcv -lhighgui -lcxcore -o macduff
 
-xyzfit: xyzfit.cpp
+xyzfit: xyzfit.cpp colorchecker.h
 	g++ -I/opt/local/include -L/opt/local/lib xyzfit.cpp -lcvaux -lcv -lhighgui -lcxcore -o xyzfit
--- macduff.cpp
+++ macduff.cpp
@@ -2,52 +2,11 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
-#define MACBETH_WIDTH   6
-#define MACBETH_HEIGHT  4
-#define MACBETH_SQUARES MACBETH_WIDTH * MACBETH_HEIGHT
+#include "colorchecker.h"
 
 #define MAX_CONTOUR_APPROX  7
 
 #define MAX_RGB_DISTANCE 444
-
-// BabelColor averages in sRGB:
-//   http://www.babelcolor.com/main_level/ColorChecker.htm
-// (converted to BGR order for comparison)
-CvScalar colorchecker_srgb[MACBETH_HEIGHT][MACBETH_WIDTH] =
-    {
-        {
-            cvScalar(67,81,115),
-            cvScalar(129,149,196),
-            cvScalar(157,123,93),
-            cvScalar(65,108,90),
-            cvScalar(176,129,130),
-            cvScalar(171,191,99)
-        },
-        {
-            cvScalar(45,123,220),
-            cvScalar(168,92,72),
-            cvScalar(98,84,195),
-            cvScalar(105,59,91),
-            cvScalar(62,189,160),
-            cvScalar(41,161,229)
-        },
-        {
-            cvScalar(147,62,43),
-            cvScalar(72,149,71),
-            cvScalar(56,48,176),
-            cvScalar(22,200,238),
-            cvScalar(150,84,188),
-            cvScalar(166,136,0)
-        },
-        {
-            cvScalar(240,245,245),
-            cvScalar(201,201,200),
-            cvScalar(161,161,160),
-            cvScalar(121,121,120),
-            cvScalar(85,84,83),
-            cvScalar(50,50,50)
-        }
-    };
 
 double euclidean_distance(CvScalar p_1, CvScalar p_2)
 {   
@@ -198,13 +157,6 @@ void * draw_colorchecker(CvMat * colorchecker_values, CvMat * colorchecker_point
         }
     }
 }
-
-struct ColorChecker {
-    double error;
-    CvMat * values;
-    CvMat * points;
-    double size;
-};
 
 ColorChecker find_colorchecker(CvSeq * quads, CvSeq * boxes, CvMemStorage *storage, IplImage *image, IplImage *original_image)
 {
